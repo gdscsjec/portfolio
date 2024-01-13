544bbcc969a7e686bb04d40e13774aa27c1521de
anishshobithps <anish.shobith19@gmail.com>
2024-01-13 21:54:58 +0530

    session 3: web
    Signed-off-by: anishshobithps <anish.shobith19@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }),
-  ]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      include: {
+        mdi: ["*"],
+      },
+    }),
+  ],
 });
